cf8ddc14590041009fce57796534f19c8bc417c5
bemrekalkan <bemre.kalkan@gmail.com>
2022-06-30 16:01:20 +0300

    handleSubmit()

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 
 const Main = () => {
   const [searchText, setSearchText] = useState("");
@@ -6,16 +7,26 @@ const Main = () => {
     setSearchText(e.target.value);
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    getWeatherDataFromApi();
+  };
+
   const getWeatherDataFromApi = async () => {
     let apiKey = process.env.REACT_APP_API_KEY;
     let units = "metric";
     let lang = "tr";
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchText}&appid=${apiKey}&units=${units}&lang=${lang}`;
+    try {
+      const response = await axios.get(url);
+      const { main, name, sys, weather } = response.data;
+      const iconUrl = `https://openweathermap.org/img/wn/${weather[0].icon}@2x.png`;
+    } catch (error) {}
   };
 
   return (
     <section className="main">
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           onChange={handleChange}
           type="text"
